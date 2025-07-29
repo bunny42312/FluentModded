d77854c79f9f2f4994b6f9534fc7f6594a34ad1a
bunny42312 <82152828+bunny42312@users.noreply.github.com>
2025-07-29 20:24:57 +0700

    Update Button.lua

--- src/Components/Button.lua
+++ src/Components/Button.lua
@@ -10,7 +10,7 @@ return function(Theme, Parent, DialogCheck)
 	local Button = {}
 
 	Button.Title = New("TextLabel", {
-		FontFace = Font.new("rbxasset://fonts/families/GothamSSm.json"),
+		FontFace = Font.new("rbxasset://fonts/families/GothamSSm.json", Enum.FontWeight.Bold, Enum.FontStyle.Normal),
 		TextColor3 = Color3.fromRGB(200, 200, 200),
 		TextSize = 14,
 		TextWrapped = true,
